23637664866b34d24d4a24046f34601f9cadf830
Irina Lerner <iri.lerner@gmail.com>
2020-06-30 21:38:09 -0300

    try to fix segfault

--- functions.c
+++ functions.c
@@ -80,7 +80,7 @@
 		info->number_individuals     = 1000;
 		info->population_size        = 1000;
 		/* The population can grow and sink. Here we estimate the grown aoround 20% */
-		info->individual_vector_size = (int)(info->number_individuals * 1.05);
+		info->individual_vector_size = (int)(info->number_individuals * 1.5);
 		info->genome_size            = 150;
 		info->reproductive_distance  = (int) floor(0.05*info->genome_size);
 		info->number_generations     = 2000;
--- makefile
+++ makefile
@@ -4,8 +4,8 @@ run : main
 	rm *.o out
 
 main :
-	#gcc -c *.c -I /usr/include/gsl/
-	#gcc *.o -lgsl -lgslcblas -lm -o out -g 
+	#gcc -g -c *.c -I /usr/include/gsl/
+	#gcc *.o -lgsl -lgslcblas -lm -o out
 	gcc -g -c *.c
 	gcc *.o -lm -o out
 
