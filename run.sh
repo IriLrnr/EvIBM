877ee162be14be7f1f385e1ad00adc4c1f978932
Irina Lerner <iri.lerner@gmail.com>
2021-02-01 11:52:40 -0300

    run for 20 simulations

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ do
 		gcc -g -c main.c source/*.c -I /usr/include/gsl/
 		gcc -ansi -pedantic -Wall *.o -lgsl -lgslcblas -lm -o out
 
-		for i in 1 2 #3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+		for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 		do 
 			/usr/bin/time -o ./data/sizes_tests/performance_$l.txt -a -f "%E; %U; %S" ./out 4 $l $i $r
 		done
