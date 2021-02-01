3ac9d5182e3dd85ab5ffbf9354813194fc23f6a5
Irina Lerner <iri.lerner@gmail.com>
2021-02-01 11:51:30 -0300

    Invert order of loops

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-for l in 100 120 150 180 200 220 250 300 350
+for r in 10 15 #different radius
 do
-	for r in 15 #different radius
+	for l in 100 120 150 180 200 220 250 300 350
 	do
-
 		l_dir=./data/sizes_tests//$r/$l
 		mkdir -p $l_dir/species
 		mkdir -p $l_dir/sizes
@@ -13,7 +12,7 @@ do
 		gcc -g -c main.c source/*.c -I /usr/include/gsl/
 		gcc -ansi -pedantic -Wall *.o -lgsl -lgslcblas -lm -o out
 
-		for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+		for i in 1 2 #3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 		do 
 			/usr/bin/time -o ./data/sizes_tests/performance_$l.txt -a -f "%E; %U; %S" ./out 4 $l $i $r
 		done
