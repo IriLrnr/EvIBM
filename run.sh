e8a71fbd271462c953a5b649715d01228a4d6bec
Isabella Basso <isabbasso@riseup.net>
2021-12-26 10:28:17 -0300

    run: fixup shellcheck warnings
    This tries to improve the existing syntax. Also adds proper SIGINT
    handling.
    
    Signed-off-by: Isabella Basso <isabbasso@riseup.net>

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+trap signal_handler INT
+signal_handler() {
+    echo ""
+    echo "Found an interruption. Aborting..."
+    exit
+}
+
 gcc-9 -g -c main.c source/*.c -I /usr/include/gsl/
 gcc-9 -ansi -pedantic -Wall ./*.o -lgsl -lgslcblas -lm -o out
 
@@ -7,18 +14,17 @@ for r in $(seq 50)
 do
 	for l in $(seq 200)
 	do
-		dir=./data/article//$r/$l
-		mkdir -p $dir/species
-		mkdir -p $dir/sizes
-		mkdir -p $dir/distances
-		mkdir -p $dir/position
-		echo "real;usr;sys" > $dir/performance_$l.txt
+		dir="./data/article/$r/$l"
+		mkdir -p "$dir/{species,sizes,distances,position}"
 
+		echo "real;usr;sys" > "$dir/performance_$l.txt"
 
 		for i in $(seq -f "%02g" 1)
 		do 
-			/usr/bin/time -o ./data/article/$r/$l/performance_$l.txt -a -f "%E; %U; %S" ./out 4 $l $i $r
+			$(which time) -o "$dir/performance_$l.txt" \
+			    -a -f "%E; %U; %S" ./out 4 "$l" "$i" "$r"
 		done
 	done
 done
-rm *.o out
+
+rm ./*.o out
