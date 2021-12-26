fc56a64ccf272fc30e0893dadf41b2eb7687520f
Irina Lerner <iri.lerner@gmail.com>
2021-12-26 09:30:30 -0300

    Define GLOBAL_RNG once

--- include/random.h
+++ include/random.h
@@ -2,6 +2,8 @@
 #include <gsl_randist.h>
 #include <gsl_rng.h>
 
+#ifndef RNG_DEF
+#define RNG_DEF
 /* 
 	State-keeper of the random number generator
 */
@@ -21,3 +23,5 @@ int rand_upto (int);
 	returns a random integer from 1 to n
 */
 int rand_1to (int);
+
+#endif
